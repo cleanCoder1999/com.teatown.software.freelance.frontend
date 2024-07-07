e18395640cbbeda170ae164a1742ef62f3a79736
Lucas Müllner <lucasmuellner@gmx.at>
2024-07-07 19:00:09 +0200

    removes NxWelcomeComponent from AppComponent imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { LayoutComponent } from './layout/layout.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule, LayoutComponent],
+  imports: [
+    RouterModule,
+    LayoutComponent,
+  ],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
