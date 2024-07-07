bd27595ce758af3e95704fde4faee704d0760583
Lucas Müllner <lucasmuellner@gmx.at>
2024-07-07 19:03:00 +0200

    adds 'scroll to top' functionality to LayoutComponent

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,11 +1,26 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'teatown-layout',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './layout.component.html',
-  styleUrl: './layout.component.css'
+  styleUrl: './layout.component.scss'
 })
-export class LayoutComponent {}
+export class LayoutComponent implements OnInit {
+  windowScrolled = false;
+
+  ngOnInit() {
+    if (typeof window !== 'undefined') {
+      window.addEventListener('scroll', () => {
+        this.windowScrolled = window.pageYOffset !== 0;
+      });
+    }
+  }
+
+  scrollToTop(): void {
+    // todo this also works if the scroll behaviour is horizontal => perfect for Cody's trip
+    window.scrollTo({ left: 0, top: 0, behavior: 'smooth' });
+  }
+}
