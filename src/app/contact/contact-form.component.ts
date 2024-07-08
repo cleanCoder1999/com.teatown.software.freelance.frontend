babe4aa9575c3b24a69d8d5d79f624086988a463
Lucas Müllner <lucasmuellner@gmx.at>
2024-07-08 10:07:57 +0200

    defines contactForm of contact-form.component

--- src/app/contact/contact-form.component.ts
+++ src/app/contact/contact-form.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'teatown-contact-form',
@@ -8,4 +9,13 @@ import { Component } from '@angular/core';
   templateUrl: './contact-form.component.html',
   styleUrl: './contact-form.component.scss'
 })
-export class ContactFormComponent {}
+export class ContactFormComponent {
+  contactForm = this.formBuilder.group({
+    email: [null, [Validators.required]],
+    name: [null, [Validators.required]],
+    message: [null, [Validators.required]]
+  });
+
+  constructor(private formBuilder: FormBuilder) {
+  }
+}
