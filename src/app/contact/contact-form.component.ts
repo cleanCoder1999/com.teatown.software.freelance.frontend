57eb0ed16e2d14a07a35eb48548401a892cb6e34
Lucas Müllner <lucasmuellner@gmx.at>
2024-07-08 09:59:33 +0200

    introduces contact-form component

---
+++ src/app/contact/contact-form.component.ts
@@ -0,0 +1,11 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'teatown-contact-form',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './contact-form.component.html',
+  styleUrl: './contact-form.component.scss'
+})
+export class ContactFormComponent {}
