ef7d67327cf21b62d207a36ebcce7cf84b8c6433
Lucas Müllner <lucasmuellner@gmx.at>
2024-07-08 10:22:00 +0200

    adds CustomErrorStateMatcher and imports to contact-form.component

--- src/app/contact/contact-form.component.ts
+++ src/app/contact/contact-form.component.ts
@@ -1,20 +1,45 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroupDirective, NgForm, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ErrorStateMatcher } from '@angular/material/core';
+import { MatError, MatFormField, MatHint, MatLabel } from '@angular/material/form-field';
+import { MatInput } from '@angular/material/input';
+
+/** Error when invalid control is dirty, touched, or submitted. */
+export class CustomErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(
+    control: FormControl | null,
+    form: FormGroupDirective | NgForm | null
+  ): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid &&
+      (control.dirty || control.touched || isSubmitted));
+  }
+}
 
 @Component({
   selector: 'teatown-contact-form',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MatFormField,
+    MatInput,
+    ReactiveFormsModule,
+    MatLabel,
+    MatError,
+    MatHint,
+  ],
   templateUrl: './contact-form.component.html',
   styleUrl: './contact-form.component.scss'
 })
 export class ContactFormComponent {
   contactForm = this.formBuilder.group({
-    email: [null, [Validators.required]],
-    name: [null, [Validators.required]],
-    message: [null, [Validators.required]]
+    email: ['', [Validators.required]],
+    name: ['', [Validators.required]],
+    message: ['', [Validators.required]]
   });
+
+  errorStateMatcher = new CustomErrorStateMatcher();
 
   constructor(private formBuilder: FormBuilder) {
   }
