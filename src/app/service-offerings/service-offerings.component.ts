8249ba086aa1bf602ca413206c23913fc3fe9391
Lucas Müllner <lucasmuellner@gmx.at>
2024-07-13 12:05:15 +0200

    introduces service-offerings.component

---
+++ src/app/service-offerings/service-offerings.component.ts
@@ -0,0 +1,11 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'teatown-service-offerings',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './service-offerings.component.html',
+  styleUrl: './service-offerings.component.scss'
+})
+export class ServiceOfferingsComponent {}
