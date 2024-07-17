f9ab874d2ffda97aae274c212606a3ede6487c83
Lucas Müllner <lucasmuellner@gmx.at>
2024-07-17 12:58:13 +0200

    uses ScrollAnimationDirective in service-offerings.component

--- src/app/service-offerings/service-offerings.component.ts
+++ src/app/service-offerings/service-offerings.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ScrollAnimationDirective } from '../directives/ScrollAnimationDirective';
 
 @Component({
   selector: 'teatown-service-offerings',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ScrollAnimationDirective],
   templateUrl: './service-offerings.component.html',
   styleUrl: './service-offerings.component.scss'
 })
